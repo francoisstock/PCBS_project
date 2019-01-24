3d17b7d237171db1afb91a64305c4d211a300fd5
francoisstock <45467216+francoisstock@users.noreply.github.com>
2019-01-24 11:53:04 +0100

    Update adaptation_induced_blindness.py

--- adaptation_induced_blindness.py
+++ adaptation_induced_blindness.py
@@ -12,15 +12,16 @@ def positionGabors(degree, n=8):
         positions.append([degree*cos(i*2*pi/n), degree*sin(i*2*pi/n)])
     return positions
 
-def movingGabors(fix, gabors, pos, freq, rate, sec):
+def movingGabors(win, fix, gabors, freq, rate, sec):
     """ Draws drifting Gabors at predefined frequency"""
     for frameN in range(sec*rate):
-        gabors.phases += freq/rate
-        gabors.draw()
+        for i in range(len(gabors)):
+            gabors[i].setPhase(freq/rate, '+')
+            gabors[i].draw()
         fix.draw()
-        experiment_window.flip()
+        win.flip()
 
-def contrastGabor(fix, gabor, direction, rate, sec):
+def contrastGabor(win, fix, gabor, direction, rate, sec):
     for frameN in range(sec*rate):
         if direction == 'onset':
             gabor.setContrast(newContrast = (frameN+1)/(sec*rate))
@@ -28,7 +29,7 @@ def contrastGabor(fix, gabor, direction, rate, sec):
             gabor.setContrast(newContrast = (1 - (frameN+1)/(sec*rate)))
         gabor.draw()
         fix.draw()
-        experiment_window.flip()
+        win.flip()
 
 
 trial_timer = core.Clock()
@@ -41,32 +42,33 @@ experiment_window = visual.Window([1366,768], allowGUI=True,
 
 # Create TrialHandler for 16 conditions:
 targetResponses = []
-targetPositions = positionGabors(degree=6)
 for i in range(nConditions):
-    position = targetPositions[i%8]
-    correctResponse = 'space'
     if i < 8:
         orientation = 0 # vertical
     else:
         orientation = 90 # horizontal
-    targetResponses.append({'Orientation': orientation, 'Position': position,
-        'CorrectResponse':correctResponse})
+    positionNumber = positionGabors(degree=6)[i%8]
+    correctResponse = 'space'
+    targetResponses.append({'Orientation': orientation,
+        'Position': positionNumber, 'CorrectResponse':correctResponse})
 trials = data.TrialHandler(targetResponses, nTrials/nConditions,
     method='random')
 
 # Create stimuli
-adaptors = visual.ElementArrayStim(win=experiment_window, units='deg',
-    fieldPos= positionGabors(degree=6), nElements=8, sizes=2.0, sfs=1.5,
-    contrs=1, phases=0.5, elementMask='gauss') # generates 8 Gabors
-target = visual.GratingStim(experiment_window, sf=1.5, size=2,
+gabor = visual.GratingStim(experiment_window, sf=1.4, size=2,
     mask='gauss', ori=0, phase= 0.5, contrast=1)
+adaptors = []
+for i in range(8):
+    adaptor = visual.GratingStim(experiment_window, sf=1.4, size=2,mask='gauss',
+        ori=0, phase= 0.5, contrast=1, pos = positionGabors(degree=6)[i])
+    adaptors.append(adaptor)
 fixation = visual.TextStim(experiment_window,text=('+'),
     alignHoriz="center", color = 'white')
 
 # display instructions and wait for key press
 message1 = visual.TextStim(experiment_window, pos=[0,+3],text='Hit a key when ready.')
 message2 = visual.TextStim(experiment_window, pos=[0,-3],
-    text='Look at the fixation cross /nPress space key when you see target')
+    text='Look at the fixation cross Press space key when you see target')
 message1.draw()
 message2.draw()
 fixation.draw()
@@ -74,25 +76,22 @@ experiment_window.flip()
 event.waitKeys()
 
 # First adaptation period
-movingGabors(fixation, adaptors, pos = positionGabors(degree=6),
-    rate=refRate, freq=8, sec=3)
+movingGabors(experiment_window, fixation, adaptors, rate=refRate,
+    freq=8, sec=20)
 
 # Trials
 for trial in trials:
     # Re-adaptation
-    movingGabors(fixation, adaptors, pos = positionGabors(degree=6),
-        rate=refRate, freq=8, sec=1)
-
-    #for frameN in range(0.4*refRate): # Wait for 400ms --> explain in report
-    fixation.draw()
-    experiment_window.flip()
-    core.wait(0.4)
+    movingGabors(experiment_window, fixation, adaptors, rate=refRate,
+        freq=8, sec=5)
 
     # Target
-    target.setPos(newPos = trial['Position'])
-    target.setOri(newOri = trial['Orientation'])
-    contrastGabor(fixation, target, direction='onset', rate=refRate, sec=1)
-    contrastGabor(fixation, target, direction='offset', rate=refRate, sec=3)
+    gabor.setPos(newPos = trial['Position'])
+    gabor.setOri(newOri = trial['Orientation'])
+    contrastGabor(experiment_window,fixation, gabor, direction='onset',
+        rate=refRate, sec=1)
+    contrastGabor(experiment_window, fixation, gabor, direction='offset',
+        rate=refRate, sec=1)
 
 core.wait(1)
 
